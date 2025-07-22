25a857e55a4a4e1df6b268134a853a89de5e77f7
kms3691h <kms3691h@gmail.com>
2025-07-22 11:49:31 +0900

    Implement async main function and update dependencies in Cargo.toml

--- test_staad/src/main.rs
+++ test_staad/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+mod staad;
+use staad::process::StaadProcess;
+
+#[tokio::main]
+async fn main() {
+    // StaadBackgroundAnalyzer
     println!("Hello, world!");
+    let mut _staad = StaadProcess::new("");
+    match _staad.start().await {
+        Err(e) => {
+            println!("{:#?}", e)
+        }
+        _ => {
+            let _ = _staad.test_code();
+        }
+    };
 }
